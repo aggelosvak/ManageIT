422762630f1aff8067115f65b4f6dc72a72ea0e9
Your Name <eevavalou@gmail.com>
2025-05-27 20:45:42 +0300

    meeting

--- src/meeting/Meeting.java
+++ src/meeting/Meeting.java
@@ -1,4 +1,45 @@
 package meeting;
 
+import java.util.List;
+
 public class Meeting {
+    private String title;
+    private String date;
+    private String time;
+    private List<Employee> participants;
+    private String description;
+    private String location;
+
+    public Meeting(String title, String date, String time, List<Employee> participants, String description, String location) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.participants = participants;
+        this.description = description;
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public List<Employee> getParticipants() {
+        return participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
